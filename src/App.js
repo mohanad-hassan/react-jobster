8a2ba0e3cf6efb0a91f35043c58115bc44eab878
mohanad hassan <mohanadhassan969696@gmail.com>
2023-04-16 19:07:30 +0300

    FormRow Register react-tostify

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Landing, Register, Dashboard, Error } from "./pages";
+  import { ToastContainer } from "react-toastify";
+  import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>JOBSTER </h1>
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/landing" element={<Landing />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/error" element={<Error />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </div>
+    <ToastContainer position="top-center"/>
     </BrowserRouter>
   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,86 @@
-import React from 'react'
+import { useState, } from "react";
+import { Logo,FormRow } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+  import {  toast } from "react-toastify";
 
-const Register = () => {
+// redux toolkit and useNavigate later
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: false,
+};
+// if possible prefer local state
+// global state
+
+function Register() {
+  const [values, setValues] = useState(initialState);
+
+  // redux toolkit and useNavigate later
+
+  const handleChange = (e) => {
+    const name  = e.target.name
+    const value = e.target.value
+    setValues({...values,[name]:value})
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const {email,password,name,isMember} = values
+    if (!email || !password || (!name && !isMember)) {
+      toast.warning("please provide all values ");
+    }
+  };
+
+  const toggleMember = () => { setValues({...initialState,isMember:!values.isMember}) }
+
+
   return (
-    <div>Register</div>
-  )
-}
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
 
-export default Register
+        {/* name field */}
+        <div className="form-row">
+          {/* name field */}
+          {!values.isMember && (
+            <FormRow
+              type="text"
+              name="name"
+              value={values.name}
+              handleChange={handleChange}
+            />
+          )}
+          {/* email field */}
+          <FormRow
+            type="email"
+            name="email"
+            value={values.email}
+            handleChange={handleChange}
+          />
+          {/* password field */}
+          <FormRow
+            type="password"
+            name="password"
+            value={values.password}
+            handleChange={handleChange}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
+}
+export default Register;
