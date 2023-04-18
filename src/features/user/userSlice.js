725d1f4d5e285a68ad5497ab72a4457e70c69ff8
mohanad hassan <mohanadhassan969696@gmail.com>
2023-04-18 18:42:29 +0300

    add-job first stage

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -13,36 +13,60 @@ const initialState = {
 };
 
 export const registerUser = createAsyncThunk(
-  'user/registerUser',
+  "user/registerUser",
   async (user, thunkAPI) => {
-   // console.log(`Register User : ${JSON.stringify(user)}`)
-      try {
-        const resp = await customFetch.post("/auth/register", user);
-        return resp.data;
-      } catch (error) {
-        return thunkAPI.rejectWithValue(error.response.data.msg);
-      }
-}
+    // console.log(`Register User : ${JSON.stringify(user)}`)
+    try {
+      const resp = await customFetch.post("/auth/register", user);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+  }
 );
 export const loginUser = createAsyncThunk(
-  'user/loginUser',
+  "user/loginUser",
   async (user, thunkAPI) => {
     try {
       const resp = await customFetch.post("/auth/login", user);
       return resp.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data.msg);
-    }});
+    }
+  }
+);
 
+export const updateUser = createAsyncThunk(
+  "user/updateUser",
+  async (user, thunkAPI) => {
+    try {
+      const resp = await customFetch.patch("/auth/updateUser", user, {
+        headers: {
+          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+        },
+      });
+      return resp.data;
+    } catch (error) {
+      if (error.response.status === 401) {
+        thunkAPI.dispatch(logoutUser());
+        return thunkAPI.rejectWithValue("Unauthorized! Logging Out...");
+      }
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
 
 const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    logoutUser: (state) => {
+    logoutUser: (state,{payload}) => {
       state.user = null;
       state.isSidebarOpen = false;
       removeUserFromLocalStorage();
+        if (payload) {
+          toast.success(payload);
+        }
     },
     toggleSidebar: (state) => {
       state.isSidebarOpen = !state.isSidebarOpen;
@@ -77,8 +101,23 @@ const userSlice = createSlice({
       state.isLoading = false;
       toast.error(payload);
     },
+    [updateUser.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [updateUser.fulfilled]: (state, { payload }) => {
+      const { user } = payload;
+      state.isLoading = false;
+      state.user = user;
+
+      addUserToLocalStorage(user);
+      toast.success("User Updated");
+    },
+    [updateUser.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    },
   },
 });
 
 export default userSlice.reducer;
-export const { toggleSidebar,logoutUser } = userSlice.actions;
+export const { toggleSidebar, logoutUser } = userSlice.actions;
