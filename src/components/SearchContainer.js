5f1a7353da73441d7f7722a19f2d66cf33cfc7c5
mohanad hassan <mohanadhassan969696@gmail.com>
2023-04-18 23:02:37 +0300

    debounce search

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -2,32 +2,47 @@ import { FormRow, FormRowSelect } from ".";
 import Wrapper from "../assets/wrappers/SearchContainer";
 import { useSelector, useDispatch } from "react-redux";
 import { handleChange,clearFilters } from "../features/allJobs/allJobsSlice";
+import { useState, useMemo } from "react";
+
 const SearchContainer = () => {
   const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
     useSelector((store) => store.allJobs);
   const { jobTypeOptions, statusOptions } = useSelector((store) => store.job);
   const dispatch = useDispatch();
+  const [localSearch, setLocalSearch] = useState('');
+
   const handleSearch = (e) => {
-      if (isLoading) return;
-    const name   =e.target.name
-    const value = e.target.value
-    dispatch(handleChange({name,value}))};
+    dispatch(handleChange({ name: e.target.name, value: e.target.value }));
+  };
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(clearFilters())
   };
+   const debounce = () => {
+     let timeoutID;
+     return (e) => {
+       setLocalSearch(e.target.value);
+       clearTimeout(timeoutID);
+       timeoutID = setTimeout(() => {
+         dispatch(handleChange({ name: e.target.name, value: e.target.value }));
+       }, 1000);
+     };
+   };
+
+     const optimizedDebounce = useMemo(() => debounce(), []);
+
   return (
     <Wrapper>
       <form className="form">
         <h4>search form</h4>
         <div className="form-center">
           {/* search position */}
-
           <FormRow
             type="text"
             name="search"
-            value={search}
-            handleChange={handleSearch}
+            value={localSearch}
+            handleChange={ optimizedDebounce}
           />
           {/* search by status */}
           <FormRowSelect
